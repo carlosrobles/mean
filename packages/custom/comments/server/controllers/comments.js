9ccd4146651eedceb4e88f9741befbe3877c7da7
Carlos Robles <carlos@estudiocarlosrobles.com>
2016-01-11 21:15:28 +0800

    adding basic controller for comment, create, update, destroy, show, fetchByArticle - passing saving tests

---
+++ packages/custom/comments/server/controllers/comments.js
@@ -0,0 +1,144 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Comment = mongoose.model('Comment'),
+  User = mongoose.model('User');
+
+
+/**
+ * Find Comment by id
+ */
+exports.comment = function(req, res, next, id) {
+  Comment.load(id, function(err, comment) {
+    if (err) return next(err);
+    if (!comment) return next(new Error('Failed to load Comment ' + id));
+    req.comment = comment;
+    next();
+  });
+};
+
+
+
+/**
+ * Create a Comment
+ */
+exports.create = function(req, res) {
+  var comment = new Comment(req.body);
+   //comment.user = req.user;
+
+    comment.save(function(err) {
+    if (err) {
+      return res.json(500, {
+        error: 'Cannot save the comment'
+      });
+    }
+        /*
+        Articles.events.publish({
+            action: 'created',
+            user: {
+                name: req.user.name
+            },
+            url: config.hostname + '/articles/' + article._id,
+            name: article.title
+        });
+        */
+        res.json(comment);
+  });
+};
+
+/**
+ * Update a comment
+ */
+exports.update = function(req, res) {
+  var comment = req.comment;
+
+  comment = _.extend(comment, req.body);
+
+    comment.save(function(err) {
+        if (err) {
+            return res.status(500).json({
+                error: 'Cannot update the comment'
+            });
+        }
+/*
+        Articles.events.publish({
+            action: 'updated',
+            user: {
+                name: req.user.name
+            },
+            name: article.title,
+            url: config.hostname + '/articles/' + article._id
+        });
+*/
+        res.json(comment);
+    });
+};
+
+/**
+ * Delete an Comment
+ */
+exports.destroy = function(req, res) {
+  var comment = req.comment;
+
+  comment.remove(function(err) {
+    if (err) {
+      return res.json(500, {
+        error: 'Cannot delete the comment'
+      });
+    } else {
+
+      /*  Articles.events.publish({
+            action: 'deleted',
+            user: {
+                name: req.user.name
+            },
+            name: article.title
+        });*/
+      res.json(comment);
+    }
+  });
+};
+
+
+
+/**
+ * Show a comment
+ */
+exports.show = function(req, res) {
+    res.json(req.comment);
+};
+
+
+
+
+
+
+/**
+ * Fetching comments for a post
+ */
+exports.fetchByArticle = function(req, res) {
+  var articleId = req.params.articleId;
+  var limit = req.query.limit;
+  var query = Comment.find({
+      article: articleId
+    })
+    .sort({
+      _id: -1
+    })
+    .populate('user', 'name username');
+  if (limit) {
+    query.limit(limit);
+  }
+  query.exec(function(err, comments) {
+    if (err) {
+        return res.status(500).json({
+            error: 'Cannot list the comments'
+        });
+    } else {
+      res.json(comments);
+    }
+  });
+};
