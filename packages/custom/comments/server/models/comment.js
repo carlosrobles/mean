c89d3eed127511e5439fa622f4ab4a4ae91c3f49
Carlos Robles <carlos@estudiocarlosrobles.com>
2016-01-11 20:13:23 +0800

    created comment model

---
+++ packages/custom/comments/server/models/comment.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+
+/**
+ * Post Schema
+ */
+var CommentSchema = new Schema({
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: {
+    type: Date,
+    default: Date.now
+  },
+  content: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    index: true
+  },
+  userName: {
+    type: String
+  }
+});
+
+/**
+ * Validations
+ */
+CommentSchema.path('content').validate(function(content) {
+  return !!content;
+}, 'Comment cannot be blank');
+
+/**
+ * Statics
+ */
+CommentSchema.statics.load = function(id, cb) {
+  this.findOne({
+    _id: id
+  }).populate('user', 'name username').exec(cb);
+};
+mongoose.model('Comment', CommentSchema);
