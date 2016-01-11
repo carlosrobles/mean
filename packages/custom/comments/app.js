ba497cd492889de1bd1987e91114b36da72ffd93
Carlos Robles <carlos@estudiocarlosrobles.com>
2016-01-11 19:20:43 +0800

    removed comment sample page from menu. retained view, route and controller tho

--- packages/custom/comments/app.js
+++ packages/custom/comments/app.js
@@ -16,13 +16,6 @@ Comments.register(function(app, auth, database) {
   //We enable routing. By default the Package Object is passed to the routes
   Comments.routes(app, auth, database);
 
-  //We are adding a link to the main menu for all authenticated users
-  Comments.menus.add({
-    title: 'comments example page',
-    link: 'comments example page',
-    roles: ['authenticated'],
-    menu: 'main'
-  });
   
   Comments.aggregateAsset('css', 'comments.css');
 
