193de848646e85d2b6152e7700d66cb304f9397f
Carlos Robles <carlos@estudiocarlosrobles.com>
2016-01-11 23:35:09 +0100

    create factory for comments

--- packages/custom/comments/public/services/comments.js
+++ packages/custom/comments/public/services/comments.js
@@ -1,9 +1,14 @@
 'use strict';
 
-angular.module('mean.comments').factory('Comments', [
-    function () {
-        return {
-            name: 'comments'
-        };
+//Comments service used for articles REST endpoint
+angular.module('mean.comments').factory('CommentsResource', ['$resource',
+    function($resource) {
+        return $resource('api/comments/:commentId', {
+            commentId: '@_id'
+        }, {
+            update: {
+                method: 'PUT' //I wont implement update, just save for now.
+            }
+        });
     }
 ]);
