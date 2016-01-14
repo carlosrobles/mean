e70c8b3f618be0ca0086bd48480d274d6701036a
Carlos Robles <carlos@estudiocarlosrobles.com>
2016-01-14 11:04:59 +0100

    added article view override configuration

--- packages/custom/utils/public/config.js
+++ packages/custom/utils/public/config.js
@@ -1,4 +1,6 @@
 angular.module('mean.system').constant("utilsconfig", {
-    
+        overrides : {
+            '/articles/views/view.html': '/comments/views/override/article-view.html'
+        }
     }
 );
